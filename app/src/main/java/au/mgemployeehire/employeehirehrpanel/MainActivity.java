361cc455849bd9148cb5a93b0a9c46bec1951808
Md. Talat Mahmud <md.talatcse@gmail.com>
2021-03-02 19:01:17 +0600

    first commit master

---
+++ app/src/main/java/au/mgemployeehire/employeehirehrpanel/MainActivity.java
@@ -0,0 +1,29 @@
+package au.mgemployeehire.employeehirehrpanel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button showAllJobPostButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showAllJobPostButton = findViewById(R.id.showAllJobPostBtnID);
+
+        showAllJobPostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AllJobPostActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
